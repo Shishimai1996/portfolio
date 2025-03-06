7e37f77f67a813a2d126ea9b38a0c0a9290157ee
Mai Shimizu/舞 清水 <159525515+Shishimai1996@users.noreply.github.com>
2025-03-06 16:30:55 +0900

    delete node_modules

--- next.config.mjs
+++ next.config.mjs
@@ -2,7 +2,7 @@ const isProduction = process.env.NODE_ENV === 'production';
 
 export default {
   reactStrictMode: true,
-  // output: "export",
+  output: "export",
   images: {
     unoptimized: true,
   },
