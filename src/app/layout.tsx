93b9bbc3a1fcea5675429d10fbd7c1998d299b4e
Mai Shimizu/舞 清水 <159525515+Shishimai1996@users.noreply.github.com>
2025-07-24 22:27:38 +0900

    new icon

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -13,6 +13,9 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <head>
+        <link rel="icon" href="/favicon.png" type="image/png" />
+      </head>
       <body>
         <Box sx={{ width: "100%" }}> {children}</Box>
       </body>
