92118a0bf1b8c3a68946eec57a506a25a13dbab8
清水舞 <s.amandamy2161@gmail.com>
2026-04-19 00:15:16 +0900

    fix config

--- next.config.mjs
+++ next.config.mjs
@@ -2,7 +2,7 @@ const isProduction = process.env.NODE_ENV === "production";
 
 const nextConfig = {
   reactStrictMode: true,
-  // output: "export",
+  output: "export",
   images: {
     unoptimized: true,
   },
